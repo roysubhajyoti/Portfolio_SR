728ad77dbeb74b76b8f33407a4655153a565f18b
roysubhajyoti <roysubbhajyoti272@gmail.com>
2024-12-12 02:11:59 +0530

    add on hover glow

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { CONTACTS } from "../constants/Info";
 
 export const Navbar = () => {
   return (
@@ -8,8 +9,20 @@ export const Navbar = () => {
         SR
       </div>
       <div className=" m-8 flex items-center justify-center gap-4 text-xl">
-        <FaGithub className="text-violet-50" />
-        <FaLinkedin className="text-violet-50" />
+        <a
+          href={CONTACTS.gitHUb}
+          target="_blank"
+          className="hover:shadow-[0_0_10px_rgba(255,255,255,0.8)] hover:text-white transition-shadow duration-300 rounded-full"
+        >
+          <FaGithub className="text-violet-50" />
+        </a>
+        <a
+          href={CONTACTS.ldin}
+          target="_blank"
+          className="hover:shadow-[0_0_10px_rgba(255,255,255,0.8)] hover:text-white transition-shadow duration-300 rounded "
+        >
+          <FaLinkedin className="text-violet-50" />
+        </a>
       </div>
     </nav>
   );
